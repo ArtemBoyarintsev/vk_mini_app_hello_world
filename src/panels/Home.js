76ec338a8714cdae728be5af2f3f48efbd9b8c43
Artem <boyarintsev95@gmail.com>
2020-05-03 21:19:25 +0700

    Implemented some new features

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -8,10 +8,25 @@ import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
+import pan from '../img/pan_with_food.jpg';
+import './container.css';
+
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
-		{fetchedUser &&
+		<PanelHeader>Еда и нутриенты</PanelHeader>
+		<div class="container">
+			<img src={pan} alt="Norway" style={{height : '100%'}}/>
+				<div class="content">
+					<p>Почему это важно?</p>
+					<p>Мы привыкли думать, что автомобили, самолеты, новомодный искусственный интеллект - это сложно устроенные вещи.</p>
+					<p>Но что касается нашего организма? - Ни для кого не новость, что наш мозг превосходит по сложности всё перечисленное.</p>
+					<p>Но почему-то топливо для нашего организма - еда и его переработка в необходимые для организма вещества обделяется вниманием</p>
+					<p>Оставаясь с нами, вы сможете узнать много новой информации о необходимых витаминах, минералах. Узнаете о продуктах, богатых ими.</p>
+				</div>
+		</div>
+		
+		{/*fetchedUser && 
+		
 		<Group title="User Data Fetched with VK Bridge">
 			<Cell
 				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
@@ -19,7 +34,7 @@ const Home = ({ id, go, fetchedUser }) => (
 			>
 				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
 			</Cell>
-		</Group>}
+		</Group>*/}
 
 		<Group title="Navigation Example">
 			<Div>
